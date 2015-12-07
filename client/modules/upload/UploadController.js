5b88b539547bb430fc39d4ad5815f6945ceb80f0
Mithun Das <mithundas@Mithuns-MacBook-Pro.local>
2015-12-06 22:28:47 -0500

    upload page created

--- client/modules/upload/UploadController.js
+++ client/modules/upload/UploadController.js
@@ -3,7 +3,28 @@
  */
 appModule.controller("UploadController",["$scope","$rootScope","$log","$modal","$state", "$interval",function($scope,$rootScope,$log,$modal,$state,$interval){
 
+    $scope.totalPhoto = 0;
     $scope.imageBag =[{},{},{},{},{},{}];
     $scope.formats = ["9x13 cm","10x13 cm","11x15 cm","13x18 cm","20x30 cm","30x48cm"];
+
+    $scope.increaseQuantity = function(m){
+        m.quantity = m.quantity+1;
+        $scope.totalPhoto++
+    }
+
+    $scope.decreaseeQuantity = function(m){
+        if(m.quantity >0 ) {
+            m.quantity = m.quantity - 1;
+            $scope.totalPhoto--;
+        }
+    }
+
+    $scope.deleteImage = function($index){
+        $log.debug("image deleted");
+    }
+
+    $scope.uploadImage = function(){
+
+    }
 
 }]);
