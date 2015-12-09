4b1bf7b531d92cc334270b3f60fe394ed73484bc
Mithun Das <mithundas@Mithuns-MacBook-Pro.local>
2015-12-09 00:51:55 -0500

    heroku config

--- server/database/connectionPool.js
+++ server/database/connectionPool.js
@@ -3,17 +3,18 @@
  */
 var mysql = require('mysql');
 
-var app = require('../../app');
-//mysql://b61afe5470d800:c3a8a201@us-cdbr-iron-east-03.cleardb.net/heroku_30c4b4e9dd38c02?reconnect=true
-var config ={
-    host     : 'us-cdbr-iron-east-03.cleardb.net',
-    user     : 'b61afe5470d800',
-    password : 'c3a8a201',
-    database : 'heroku_30c4b4e9dd38c02',
-    connectionLimit: 10
-};
+var express = require('express');
+var app = express();
+var config = require('./config.json')[app.get('env')];
 
 
-var pool = mysql.createPool(config);
+
+var pool = mysql.createPool({
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    database : config.database,
+    connectionLimit: config.connectionLimit
+});
 
 module.exports = pool;
