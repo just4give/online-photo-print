46c81c1208fa9b981bf4b2f3f5904cacaa215fa8
Mithun Das <mithundas@MacBook-Pro.local>
2015-12-13 22:38:09 -0500

    image quality

--- client/modules/checkout/CheckoutController.js
+++ client/modules/checkout/CheckoutController.js
@@ -1,8 +1,8 @@
 /**
  * Created by Mithun.Das on 12/7/2015.
  */
-appModule.controller("CheckoutController",["$scope","$rootScope","$log","$modal","$state", "$interval","$confirm","OrderService",
-    function($scope,$rootScope,$log,$modal,$state,$interval,$confirm,OrderService){
+appModule.controller("CheckoutController",["$scope","$rootScope","$log","$modal","$state", "$interval","$confirm","OrderService","PhotoService",
+    function($scope,$rootScope,$log,$modal,$state,$interval,$confirm,OrderService,PhotoService){
 
     $log.debug('initializing CheckoutController');
 
